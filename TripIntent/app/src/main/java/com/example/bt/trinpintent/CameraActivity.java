548f4e07943bfad1e7480d7e93c9ccb078b35a66
Unknown <ursbhavishya@gmail.com>
2016-09-23 23:08:03 -0700

    Commint#1

---
+++ TripIntent/app/src/main/java/com/example/bt/trinpintent/CameraActivity.java
@@ -0,0 +1,45 @@
+package com.example.bt.trinpintent;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+/**
+ * Created by BT on 9/23/16.
+ */
+
+
+public class CameraActivity extends Activity {
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView imageView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camactivitylayout);
+        this.imageView = (ImageView) this.findViewById(R.id.imageView1);
+        Button photoButton = (Button) this.findViewById(R.id.button1);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+    }
+
+
+}
