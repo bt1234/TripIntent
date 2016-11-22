e233887dc25ad8f6b488cf7aa917dd060ddc26da
Unknown <ursbhavishya@gmail.com>
2016-11-22 10:06:27 -0800

    Sign up page added
    TripIntent1.2

--- TripIntent1.1/app/build/generated/source/r/debug/com/example/android/tripIntent/R.java
+++ TripIntent1.1/app/build/generated/source/r/debug/com/example/android/tripIntent/R.java
@@ -10,19 +10,41 @@ package com.example.android.tripIntent;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int pager=0x7f050000;
+        public static final int email=0x7f060003;
+        public static final int email_login_form=0x7f060002;
+        public static final int email_sign_in_button=0x7f060006;
+        public static final int login=0x7f060005;
+        public static final int login_form=0x7f060001;
+        public static final int login_progress=0x7f060000;
+        public static final int pager=0x7f060007;
+        public static final int password=0x7f060004;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int chat=0x7f030001;
-        public static final int map_section=0x7f030002;
-        public static final int photo_section=0x7f030003;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int chat=0x7f030002;
+        public static final int map_section=0x7f030003;
+        public static final int photo_section=0x7f030004;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int action_sign_in=0x7f050000;
+        public static final int action_sign_in_short=0x7f050001;
+        public static final int app_name=0x7f050002;
+        public static final int error_field_required=0x7f050003;
+        public static final int error_incorrect_password=0x7f050004;
+        public static final int error_invalid_email=0x7f050005;
+        public static final int error_invalid_password=0x7f050006;
+        public static final int permission_rationale=0x7f050007;
+        public static final int prompt_email=0x7f050008;
+        public static final int prompt_password=0x7f050009;
+        public static final int title_activity_login=0x7f05000a;
     }
 }
